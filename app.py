a8203b2ee396fbc438345027876aee7c7851530a
survelanjkns-cell <survelanjkns@moh.gov.my>
2026-05-08 11:24:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-sy nak sizing column table jadual 2.1 ikut dalam gambar. ni script sy sekarang : import streamlit as st
+import streamlit as st
 import pandas as pd
 from datetime import datetime, date, timedelta
 import pytz
