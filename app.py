6ae7dca866ebcc102c33dccea86522b759c5d739
survelanjkns-cell <survelanjkns@moh.gov.my>
2026-05-15 08:32:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -491,7 +491,9 @@ def generate_docx(matrix_df, col_sums, wabak_df, vector_df, bkk_table_df, is_bkk
             elif c_idx == bkk_table_df.shape[1]-2: set_cell_background(cells[c_idx], "FFFFB3")
             elif c_idx == bkk_table_df.shape[1]-1: set_cell_background(cells[c_idx], "E2EFDA")
 
-
+    doc.add_paragraph()
+    add_pkd_note(doc)
+    doc.add_paragraph()
 
     doc.add_paragraph() 
     sig_table = doc.add_table(rows=8, cols=3)
@@ -507,9 +509,6 @@ def generate_docx(matrix_df, col_sums, wabak_df, vector_df, bkk_table_df, is_bkk
         p_colon = row[1].paragraphs[0]
         apply_font(p_colon.add_run(":"), 11, bold=False)
 
-    doc.add_paragraph()
-    add_pkd_note(doc)
-    doc.add_paragraph()
     
     fill_sig_row(0, "Disediakan")
     fill_sig_row(1, "Jawatan")
