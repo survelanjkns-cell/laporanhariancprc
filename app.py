ca392f7aa16a19408bf57b44eb3e201dfc697736
survelanjkns-cell <survelanjkns@moh.gov.my>
2026-05-22 17:54:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -598,7 +598,7 @@ def generate_docx(matrix_df, col_sums, wabak_df, vector_df, bkk_table_df, is_bkk
 
     apply_font(h41.add_run(h41_text), 11, bold=False)
 
-    add_table_title(doc, "Jadual 4.1", "Jumlah Kejadian Bencana, Kecemasan dan Krisis (BKK) di Selangor Pada Tahun 2026")
+    add_table_title(doc, "Jadual 4.1", "Jumlah Kejadian Bencana, Kecemasan dan Krisis (BKK) di Selangor Pada Tahun 2026 Mengikut Daerah")
     t4 = doc.add_table(rows=len(bkk_table_df) + 1, cols=len(bkk_table_df.columns))
     t4.style = 'Table Grid'
     t4.width = content_width 
