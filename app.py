a90b162d1f649b5e6f23aa371e20cbc81558e471
survelanjkns-cell <survelanjkns@moh.gov.my>
2026-05-22 17:56:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def generate_docx(matrix_df, col_sums, wabak_df, vector_df, bkk_table_df, is_bkk
 
     # --- 1.0 Ringkasan Laporan Input e-Notifikasi ---
     p1_head = doc.add_paragraph()
-    apply_font(p1_head.add_run("1.0 Ringkasan Laporan Input Enotifikasi"), 11, bold=True)
+    apply_font(p1_head.add_run("1.0 Ringkasan Laporan Input e-Notifikasi"), 11, bold=True)
     
     total_notifications = int(col_sums['Grand Total'])
     h11 = doc.add_paragraph()
