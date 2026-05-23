efcc1e2bb7b3a2f29d85066f2a6452beaf775cc0
survelanjkns-cell <survelanjkns@moh.gov.my>
2026-05-23 10:35:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -574,7 +574,7 @@ def generate_docx(matrix_df, col_sums, wabak_df, vector_df, bkk_table_df, is_bkk
 
                 if kem_val > 0:
                     if kem_val == 1:
-                        status_str = f"{mangsa_prefix} dalam kejadian tersebut dengan seorang kematian dilaporkan."
+                        status_str = f"{mangsa_prefix} dalam kejadian tersebut dengan satu (1) kematian dilaporkan."
                     else:
                         status_str = f"{mangsa_prefix} dalam kejadian tersebut dengan {kem_str_formatted} kematian dilaporkan."
                 else:
